f23e035477b824639c909b83d99605be9f68488e
miguel cosano <miguel.cosano@babelgroup.com>
2023-12-31 02:08:37 +0100

    Added oauth basic functionality.

--- carbono-module/controller/carbonoController.js
+++ carbono-module/controller/carbonoController.js
@@ -1,10 +1,20 @@
 const ServiceCarbono = require('../service/carbonoService');
 const serviceCarbono = new ServiceCarbono();
 
+const axios = require("axios");
 const getCoordinatesFromPostalCode = async (req, res, next) => {
     try {
-        const coordenadas = await serviceCarbono.getCoordenadasByCodPostal(req.query.codPostal)
-        res.status(200).json(coordenadas)
+        let checkToken = await axios.get("http://127.0.0.1:5003/api/v2/usuarios/checkLocalCache",{
+            headers: {
+                'authorization': req.headers.authorization
+            }
+        })
+        if(checkToken.status !== 200){
+            res.status(checkToken.status).send(checkToken.data)
+        }else {
+            const coordenadas = await serviceCarbono.getCoordenadasByCodPostal(req.query.codPostal)
+            res.status(200).json(coordenadas)
+        }
     } catch (error) {
         res.status(401).send({success: false, message: 'No se ha podido obtener las coordenadas para el código postal ' + req.query.codPostal});
     }
@@ -12,8 +22,17 @@ const getCoordinatesFromPostalCode = async (req, res, next) => {
 
 const getHuellaCarbono = async (req, res) => {
     try {
+        let checkToken = await axios.get("http://127.0.0.1:5003/api/v2/usuarios/checkLocalCache",{
+            headers: {
+                'authorization': req.headers.authorization
+            }
+        })
+        if(checkToken.status !== 200){
+            res.status(checkToken.status).send(checkToken.data)
+        }else{
         const huella = await serviceCarbono.getHuellaCarbono(req.query.userLat,req.query.userLong, req.query.codPostalProducto);
         res.status(200).send(huella);
+        }
     } catch (error) {
         res.status(500).send({success: false, message: error.message});
     }
